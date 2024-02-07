91b987bdffb9eb58153c7c02720e19a0eab28e2f
Tharindu Dilshan <153268657+jdtdjayasingha@users.noreply.github.com>
2024-02-07 23:17:54 +0530

    Created text input field

--- lib/main.dart
+++ lib/main.dart
@@ -18,18 +18,62 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       title: "Login Screen",
       home: Scaffold(
-        body: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 50),
-                  child: Image.asset("Assets/Images/Logo.png"),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 50),
+                    child: Image.asset("Assets/Images/Logo.png"),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 30,
+                      top: 100,
+                    ),
+                    child: Text(
+                      "Welcome back",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 253, 59, 96),
+                        fontSize: 30,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 30,
+                  right: 30,
                 ),
-              ],
-            ),
-          ],
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Email/Phone Number',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 30,
+                  right: 30,
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
